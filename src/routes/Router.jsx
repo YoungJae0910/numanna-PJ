0ad3d104d82a43fdd7b071387eccbf7234edf498
Eidos <dudwns5013@gmail.com>
2023-01-20 19:27:10 +0900

    test

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Survey from '../components/Survey/Survey';
-import Header from '../page/Header';
-import Footer from '../page/Footer';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Survey from "../components/Survey/Survey";
+import Header from "../page/Header";
+import Footer from "../page/Footer";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/survey' element={<Survey />} />
+        <Route path="/survey" element={<Survey />} />
       </Routes>
       <Footer />
     </BrowserRouter>
