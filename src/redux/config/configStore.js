b772cabffe0705594aa9c58f0faa98e6b9532d4a
(yooyoungjae) <(dbdudwo0910@gmail.com)>
2023-01-21 15:07:35 +0900

    axios quest

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../modules/userSlice";
+import questSlice from "../modules/questSlice";
 
 const store = configureStore({
-  reducer: { userSlice: userSlice },
+  reducer: { userSlice: userSlice, questSlice: questSlice },
 });
 
 export default store;
