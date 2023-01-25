7d6a9f5797b44457606210a641316ba0a26e7768
(yooyoungjae) <(dbdudwo0910@gmail.com)>
2023-01-25 19:18:18 +0900

    refresh

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,7 +6,7 @@ import SignUp from "../components/SignUp/SignUp"
 import Matching from "../components/Matching/Matching"
 import Login from "../components/Login/Login"
 import Map from "../components/Map/Map"
-import Partner from "../components/Partner/Partner";
+import Partner from "../components/Partner/Partner"
 
 export default function Router() {
     return (
@@ -14,7 +14,7 @@ export default function Router() {
             <Routes>
                 <Route path="/survey" element={<Survey />} />
                 <Route path="/" element={<Main />} />
-                <Route path="/matching" element={<Matching />} />
+                <Route path="/matching/:id" element={<Matching />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/map" element={<Map />} />
