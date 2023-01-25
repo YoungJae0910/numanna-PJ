4632431208b4c2c8ee0897babb0904e8cfbe0d9c
Eidos <dudwns5013@gmail.com>
2023-01-25 12:26:47 +0900

    partner infiniteScroll

--- src/components/Partner/Partner.jsx
+++ src/components/Partner/Partner.jsx
@@ -1,92 +1,78 @@
-import React from "react";
-import styled from "styled-components";
+import React from "react"
+import { useState } from "react"
+import styled from "styled-components"
+import InfiniteScroll from "react-infinite-scroll-component"
 
 const Partner = () => {
-  return (
-    <FlexBox>
-      <PartnerUl>
-        <PartnerLi>
-          <PartnerImg></PartnerImg>
-          <PartnerInfo>
-            <span>이름:</span>
-            <span>나이:</span>
-            <span>성별:</span>
-          </PartnerInfo>
-        </PartnerLi>
-        <PartnerLi>
-          <PartnerImg></PartnerImg>
-          <PartnerInfo>
-            <span>이름:</span>
-            <span>나이:</span>
-            <span>성별:</span>
-          </PartnerInfo>
-        </PartnerLi>
-        <PartnerLi>
-          <PartnerImg></PartnerImg>
-          <PartnerInfo>
-            <span>이름:</span>
-            <span>나이:</span>
-            <span>성별:</span>
-          </PartnerInfo>
-        </PartnerLi>
-        <PartnerLi>
-          <PartnerImg></PartnerImg>
-          <PartnerInfo>
-            <span>이름:</span>
-            <span>나이:</span>
-            <span>성별:</span>
-          </PartnerInfo>
-        </PartnerLi>
-      </PartnerUl>
-    </FlexBox>
-  );
-};
+    const [items, setItems] = useState(Array.from({ length: 40 }))
 
-export default Partner;
+    const fetchData = () => {
+        setTimeout(() => {
+            setItems(items.concat(Array.from({ length: 50 })))
+        }, 1500)
+    }
+    return (
+        <div>
+            <InfiniteScroll
+                dataLength={items.length}
+                next={fetchData}
+                hasMore={true}
+                // loader={<h4>Loading...</h4>}
+            >
+                <PartnerUl>
+                    {items.map((i, index) => (
+                        <PartnerLi key={index}>
+                            <PartnerImg></PartnerImg>
+                            <PartnerInfo>
+                                <span>이름:</span>
+                                <span>나이:</span>
+                                <span>성별:</span>
+                            </PartnerInfo>
+                        </PartnerLi>
+                    ))}
+                </PartnerUl>
+            </InfiniteScroll>
+        </div>
+    )
+}
 
-const FlexBox = styled.div`
-  width: 100%;
-  height: 700px;
-  background-color: white;
-  display: flex;
-`;
+export default Partner
 
 const PartnerUl = styled.ul`
-  width: 90%;
-  height: 90%;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+    width: 90%;
+    height: 90%;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
 const PartnerLi = styled.li`
-  width: 70%;
-  height: 100px;
-  list-style: none;
-  margin: 20px;
-  padding: 10px;
-  box-sizing: border-box;
-  border-radius: 10px;
-  border: 1px solid black;
-  background-color: #ffe1e1;
-  display: flex;
-  justify-content: space-around;
-`;
+    width: 70%;
+    height: 100px;
+    list-style: none;
+    margin: 20px;
+    padding: 10px;
+    box-sizing: border-box;
+    border-radius: 10px;
+    border: 1px solid black;
+    background-color: #ffe1e1;
+    display: flex;
+    justify-content: space-around;
+`
 
 const PartnerImg = styled.div`
-  width: 40%;
-  height: 80px;
-  background-color: #f9f2f2;
-  border-radius: 50%;
-`;
+    width: 40%;
+    height: 80px;
+    background-color: #f9f2f2;
+    border-radius: 50%;
+`
 
 const PartnerInfo = styled.div`
-  width: 40%;
-  height: 80px;
-  background-color: #ffe1e1;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-`;
-
+    width: 40%;
+    height: 80px;
+    background-color: #ffe1e1;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+`
