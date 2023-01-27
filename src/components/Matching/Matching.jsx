5107a73ecf46fd8c8a0a75ff92e010d955ef43ed
MJ Sung <themrsung@gmail.com>
2023-01-27 10:21:16 +0900

    .

--- src/components/Matching/Matching.jsx
+++ src/components/Matching/Matching.jsx
@@ -71,8 +71,6 @@ export default function Matching() {
         lowerInterval()
     }
 
-    const navigate = useNavigate()
-
     const formatContent = (content) => {
         let result = content
 
@@ -128,7 +126,6 @@ export default function Matching() {
                                 <ChatLeftText>
                                     {formatContent(c.content)}
                                 </ChatLeftText>
-
                             </ChatBoxLeftDiv>
                         )
                     })}
