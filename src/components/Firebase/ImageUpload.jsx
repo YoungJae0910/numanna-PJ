daa5e614b9aab9ef29e1827900190041c5145cbe
jini <jini9256@gmail.com>
2023-01-27 14:06:17 +0900

    feat.upload(1)

--- src/components/Firebase/ImageUpload.jsx
+++ src/components/Firebase/ImageUpload.jsx
@@ -2,16 +2,22 @@ import React, { useState } from "react"
 import { storage } from "./firebase"
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage"
 import { Avatar } from "@mui/material"
+import styled from "styled-components"
 
 export default function ImageUpload() {
     const [image, setImage] = useState(null)
     const [url, setUrl] = useState(null)
+    const fileInput = React.useRef(null)
 
     const handleImageChange = (e) => {
         if (e.target.files[0]) {
             setImage(e.target.files[0])
         }
     }
+
+    // const handleImageUpload = () => {
+    //     fileInput.current.click()
+    // }
 
     const handleSubmit = () => {
         const imageRef = ref(storage, "image")
@@ -33,13 +39,57 @@ export default function ImageUpload() {
 
     return (
         <div>
-            <Avatar
-                alt="Remy Sharp"
-                src={url}
-                sx={{ width: 150, height: 150 }}
-            />
-            <input type="file" onChange={handleImageChange} />
-            <button onClick={handleSubmit}>이미지변경</button>
+            <StImgBox>
+                <Avatar
+                    alt="Remy Sharp"
+                    src={url}
+                    sx={{ width: 150, height: 150 }}
+                    onClick={() => {
+                        fileInput.current.click()
+                    }}
+                />
+                <input
+                    type="file"
+                    onChange={handleImageChange}
+                    style={{ display: "none" }}
+                    ref={fileInput}
+                />
+            </StImgBox>
+
+            <StBtnBox>
+                {/* <StBtn onClick={handleImageUpload}>파일업로드</StBtn> */}
+                <StBtn onClick={handleSubmit}>이미지변경</StBtn>
+            </StBtnBox>
         </div>
     )
 }
+
+const StBtn = styled.button`
+    width: 40%
+    height: 10px;
+    border-radius: 5px;
+    color: #f25a5a;
+    background-color:white
+    padding: 5px;
+    margin: 5px;
+    border: 2px solid #f6809f;
+    font-weight: 600;
+    outline: none;
+    &:hover {
+        background-color: #f25a5a;
+        color: white;
+    }
+`
+
+const StBtnBox = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const StImgBox = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 150px;
+    height: 150px;
+    margin: 30px 80px 5px 80px;
+`
